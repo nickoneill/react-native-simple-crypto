7670ae2d27027ffe9efbc501b1382e56dc961286
Nick O'Neill <nick.oneill@gmail.com>
2020-04-10 15:16:21 -0700

    Make it actually work now

--- ios/RCTCrypto/lib/Hmac.m
+++ ios/RCTCrypto/lib/Hmac.m
@@ -12,7 +12,17 @@
     void* buffer = malloc(CC_SHA256_DIGEST_LENGTH);
     CCHmac(kCCHmacAlgSHA256, [keyData bytes], [keyData length], [inputData bytes], [inputData length], buffer);
     NSData *nsdata = [NSData dataWithBytesNoCopy:buffer length:CC_SHA256_DIGEST_LENGTH freeWhenDone:YES];
-    return [Shared toHex:nsdata];
+
+    // Added convert to hex string
+    NSUInteger capacity = nsdata.length * 2;
+    NSMutableString *sbuf = [NSMutableString stringWithCapacity:capacity];
+    const unsigned char *buf = nsdata.bytes;
+    NSInteger i;
+    for (i=0; i<nsdata.length; ++i) {
+      [sbuf appendFormat:@"%02X", (NSUInteger)buf[i]];
+    }
+
+    return [Shared toHex:sbuf];
 }
 
 @end
